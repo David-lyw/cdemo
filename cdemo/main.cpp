f87fc277c22ea380bb269f067a46527f8c961960
王中一玉 <David@wangzhongyiyudemacbook-pro.local>
2015-12-12 16:10:30 +0800

    Initial Commit

---
+++ cdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cdemo
+//
+//  Created by 王中一玉 on 15/12/12.
+//  Copyright © 2015年 com.lyw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
